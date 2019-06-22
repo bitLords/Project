2de2625ed28e2cee5aee9a338d455dab6075f40b
bitLoards <senali1996wijesekera@gmail.com>
2019-06-22 18:17:03 +0530

    @User register, login -FE and BE- tharaka

---
+++ BackEnd/client/src/components/UserFunctions.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export const register = newUser => {
+    return axios
+        .post('users/register', {
+            stuNumber: newUser.stuNumber,
+            name: newUser.name,
+            password: newUser.password,
+        })
+        .then(res => {
+            console.log('Registered!')
+        })
+}
+
+export const login = user => {
+    return axios
+        .post('users/login', {
+            stuNumber: user.stuNumber,
+            password: user.password
+        })
+        .then(res => {
+            localStorage.setItem('usertoken', res.data)
+            return res.data
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
