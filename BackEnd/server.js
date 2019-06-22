a3bc46893e473b8c1d294fb9309ab86f264284ba
bitLoards <senali1996wijesekera@gmail.com>
2019-06-22 13:07:24 +0530

    @node modules added

---
+++ BackEnd/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+var mongoose = require("mongoose");
+var port = process.env.PORT || 5000
+
+// require('./config/config');
+// require('./models/db');
+
+app.use(bodyParser.json());
+app.use(cors()); 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+const mongoURI = 'mongodb://localhost:27071/afreactDB'
+
+mongoose
+    .connect(mongoURI,{useNewUrlParser: true})
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
+
+var Users = require('./routes/Users')
+
+app.use('Users', Users)
+
+app.listen(port, () =>{
+    console.log("Server is running on port: "+ port)
+})
+
+
